93e454ca316f6803b3ce5a5f976b3d317867b253
simonez8 <gattisimone985@gmail.com>
2024-12-16 07:58:34 +0000

    esercizio 4

--- hello.c
+++ hello.c
@@ -10,14 +10,14 @@ int NOT(int a) {
 int AND(int a, int B) {
     // Il valore di ritorno della funzione
     // TODO aggiungere l'espressione matematica per soddisfare la porta AND
-    return 1337;
+    return a*B;
 }
 
 // Funzione per simulare la porta OR
 int OR(int a, int B) {
     // Il valore di ritorno della funzione
     // TODO aggiungere l'espressione matematica per soddisfare la porta OR
-    return 1337;
+    return a+B-a*B;
 }
 
 int main() {
